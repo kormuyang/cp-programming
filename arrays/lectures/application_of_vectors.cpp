97671df1a0cc2fc4f273ac36dc46c10c429bf068
Phantakan Thepnakorn <phantakan04@gmail.com>
2025-03-22 19:23:24 +0700

    update contents in arrays

--- arrays/lectures/application_of_vectors.cpp
+++ arrays/lectures/application_of_vectors.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
     // How can we adjust the length of an array?
+    // Static Array  -> can't adjust its length
+    // Dynamic Array -> can adjust its length
+
+    int n = 50;
+    int array_one[n];
+    for (int i = 0; i < n; i++) 
+    {
+        cout << array_one[i] << ' ';
+    }
+
+    vector<int> vector_one(n, 0);
+    for (int i : vector_one)
+    {
+        cout << i << ' ';
+    }
 
     return 0;
 }
--- arrays/lectures/understanding_iterators.cpp
+++ arrays/lectures/understanding_iterators.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
+
+// data structure
+// Hospital 
+// - parameter -> Name, Capacity, Location, Staff
+// - method -> .heal(), .sleep()
+
+// vector -> data structure
+// vector -> .begin(), .end()
 
 int main()
 {
     // An iterator is a pointer-like object that points to an element of the STL container.
+    vector<string> student_names;
+    student_names.push_back("Ohm");
+    student_names.push_back("Poom");
+    student_names.push_back("Pop");
 
+    vector<string>::iterator itr = student_names.begin();
+
+    // how can we travel through vector's element via iterator
+    for (itr = student_names.begin(); itr < student_names.end(); itr++)
+    {
+        cout << *itr << ' ';
+    }
+    cout << '\n';
+    
     return 0;
 }
