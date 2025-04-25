cf7826180fb01af906e5c6a3d90c92bb07c6d6ee
kormuyang-lab <Phantakan04@gmail.com>
2025-04-25 20:12:17 +0700

    update searching chapter

--- chapters/05-searching/README.md
+++ chapters/05-searching/README.md
@@ -1,4 +1,12 @@
 # Searching
 ## Order of Lectures
+- binary_search.cpp
+- ternary_search.cpp
+- first_and_last_index.cpp
+- count_occurence.cpp
+- missing_and_repeating_number.cpp
+- sqaure_root.cpp
+- median_of_two_sorted_arrays.cpp
+- allocate_minimum_pages.cpp
 
 ## Order of Problems
---
+++ chapters/05-searching/lectures/binary_search.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Implementation of binary search with while loop
+// Time Complexity: O(log2 n)
+// Space Complexity: O(1)
+
+int main()
+{
+    vector<int> arr = {1, 2, 3, 4, 5};
+    int n = arr.size();
+    int target = 3;
+
+    int left = 1, right = n - 1;
+    while (left <= right) {
+        int mid = (left + right) / 2;
+        // cout << "Left: " << left << ", Right: " << right << ", Mid: " << mid << endl;
+        if (arr[mid] == target) {
+            cout << "Element found at index: " << mid << endl;
+            return 0;
+        } else if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    cout << "Element not found" << endl;
+    return 0;
+}
