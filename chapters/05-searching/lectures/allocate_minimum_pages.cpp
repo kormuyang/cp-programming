67cb147040898b4aabe2c2e222f5c975da75647b
kormuyang-lab <Phantakan04@gmail.com>
2025-05-09 17:39:08 +0700

    add lectures in searching

--- chapters/05-searching/lectures/allocate_minimum_pages.cpp
+++ chapters/05-searching/lectures/allocate_minimum_pages.cpp
@@ -2,6 +2,23 @@
 
 using namespace std;
 
+/*
+Problem:
+    Given an array and an integer k, where arr[i] is the number of pages of a book and k is total number of students.
+    All the books need to be allocated to k students in contiguous manner, with each student getting at least one book.
+    The task is to minimize the maximum number of pages allocated to a student.
+    If it is not possible to allocate books to all students, return -1.
+*/
+// Time Complexity: O(n log m), where n is the number of books and m is the maximum number of pages in a book.
+// Space Complexity: O(1)
+
+// Input:
+// 4 2
+// 12 34 67 90
+
+// Output:
+// 113
+
 int main() {
     return 0;
 }
