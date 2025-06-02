afba1ed98e4a94ac04e930fb5d29226605beb4fc
kormuyang-lab <Phantakan04@gmail.com>
2025-06-02 14:59:36 +0700

    update arrays/solutions

--- chapters/04-arrays/solutions/is_an_array_sorted.cpp
+++ chapters/04-arrays/solutions/is_an_array_sorted.cpp
@@ -6,14 +6,14 @@ int main()
 {
     int n; cin >> n;
     vector<int> a(n); for (int i = 0; i < n; i++) cin >> a[i];
-    cout << a[0];
     for (int i = 1; i < n; i++)
     {
-        if (a[i] != a[i - 1])
+        if (a[i] < a[i - 1])
         {
-            cout << ' ' << a[i];
+            cout << "NO\n";
+            return 0;
         }
     }
-    cout << '\n';
+    cout << "YES\n";
     return 0;
 }
